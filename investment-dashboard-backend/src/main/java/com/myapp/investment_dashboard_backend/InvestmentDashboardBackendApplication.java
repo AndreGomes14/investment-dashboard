961e869e9609cc26389b417dbd37bf97d74262dd
angomes <angomes@pictet-technologies.com>
2025-03-03 00:37:33 +0000

    Initialize Spring Boot backend

---
+++ investment-dashboard-backend/src/main/java/com/myapp/investment_dashboard_backend/InvestmentDashboardBackendApplication.java
@@ -0,0 +1,13 @@
+package com.myapp.investment_dashboard_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestmentDashboardBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvestmentDashboardBackendApplication.class, args);
+	}
+
+}
---
+++ investment-dashboard-backend/src/test/java/com/myapp/investment_dashboard_backend/InvestmentDashboardBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myapp.investment_dashboard_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestmentDashboardBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
